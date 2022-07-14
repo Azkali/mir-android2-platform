393f094bed09f97da4202bb647464e71bb3329f8
Alfred Neumayer <dev.beidl@gmail.com>
2022-07-14 22:57:50 +0200

    include: Adapt AndroidDriverInterpreter interface
    This interface is intended to be used by both the server platform
    and the client platform to each integrate into the Android-side
    GL/EGL driver stack respectively.
    
    Some of these pure-virtual methods are not going to receive an
    implementation anytime soon, they are there to be filled later
    when someone finds the time to bring additional performance
    improvements to mir-android-platform.
    
    Other than that, adaptation of the implementations is following.

--- src/platforms/android/include/android_driver_interpreter.h
+++ src/platforms/android/include/android_driver_interpreter.h
@@ -22,6 +22,7 @@
 #include "native_buffer.h"
 #include <system/window.h>
 #include "mir/geometry/size.h"
+#include "mir/geometry/rectangles.h"
 #include <memory>
 
 namespace mir
@@ -33,11 +34,15 @@ namespace android
 class AndroidDriverInterpreter
 {
 public:
-    virtual NativeBuffer* driver_requests_buffer() = 0;
+    virtual std::shared_ptr<NativeBuffer> driver_requests_buffer(int fence) = 0;
     virtual void driver_returns_buffer(ANativeWindowBuffer*, int fence) = 0;
+    virtual void driver_cancels_buffer(ANativeWindowBuffer*, int fence) = 0;
+    virtual void lock_buffer(ANativeWindowBuffer*) = 0;
     virtual void dispatch_driver_request_format(int format) = 0;
     virtual void dispatch_driver_request_buffer_count(unsigned int count) = 0;
     virtual void dispatch_driver_request_buffer_size(geometry::Size size) = 0;
+    virtual void dispatch_driver_request_damage(geometry::Rectangles areas) = 0;
+    virtual void dispatch_driver_usage_bits(uint64_t) = 0;
     virtual int driver_requests_info(int key) const = 0;
     virtual void sync_to_display(bool sync) = 0;
 protected:

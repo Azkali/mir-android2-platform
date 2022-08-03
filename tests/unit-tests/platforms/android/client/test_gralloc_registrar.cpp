f6296e0c2e0ec449793352843dd0af005049b1a6
Ratchanan Srirattanamet <ratchanan@ubports.com>
2022-08-04 01:54:25 +0700

    tests/unit-tests: fix GrallocRegistrar test suite
    - CMakeLists.txt entry is uncomented.
    - Add missing includes.
    - Updated to use HybrisGralloc and NativeHandleOps interface.
    - A no longer relavant test is removed.

--- tests/unit-tests/platforms/android/client/test_gralloc_registrar.cpp
+++ tests/unit-tests/platforms/android/client/test_gralloc_registrar.cpp
@@ -17,9 +17,12 @@
  */
 
 #include "native_buffer.h"
+#include "hardware/gralloc.h"
 #include "mir/graphics/platform_ipc_operations.h"
 #include "src/platforms/android/client/gralloc_registrar.h"
 #include "mir/test/doubles/mock_android_native_buffer.h"
+#include "mir/test/doubles/mock_hybris_gralloc.h"
+#include <memory>
 #include <stdexcept>
 #include <fcntl.h>
 #include <gtest/gtest.h>
@@ -29,62 +32,17 @@ namespace mcla = mir::client::android;
 namespace geom = mir::geometry;
 namespace mtd = mir::test::doubles;
 
-class MockRegistrarDevice : public gralloc_module_t
+class MockNativeHandleOps : public mcla::NativeHandleOps
 {
 public:
-    MockRegistrarDevice()
-    {
-        registerBuffer = hook_registerBuffer;
-        unregisterBuffer = hook_unregisterBuffer;
-        lock = hook_lock;
-        unlock = hook_unlock;
-    }
-
-    MOCK_CONST_METHOD2(registerBuffer_interface,
-        int(struct gralloc_module_t const*, buffer_handle_t));
-    MOCK_CONST_METHOD2(unregisterBuffer_interface,
-        int(struct gralloc_module_t const*, buffer_handle_t));
-    MOCK_CONST_METHOD8(lock_interface,
-        int(struct gralloc_module_t const*, buffer_handle_t,
-            int, int, int, int, int, void**));
-    MOCK_CONST_METHOD2(unlock_interface,
-        int(struct gralloc_module_t const*, buffer_handle_t));
-
-    static int hook_registerBuffer(
-        struct gralloc_module_t const* module, buffer_handle_t handle)
-    {
-        auto registrar = static_cast<const MockRegistrarDevice*>(module);
-        return registrar->registerBuffer_interface(module, handle);
-    }
-
-    static int hook_unregisterBuffer(
-        struct gralloc_module_t const* module, buffer_handle_t handle)
-    {
-        auto registrar = static_cast<const MockRegistrarDevice*>(module);
-        return registrar->unregisterBuffer_interface(module, handle);
-    }
-
-    static int hook_lock(
-        struct gralloc_module_t const* module, buffer_handle_t handle,
-        int usage, int l, int t, int w, int h, void** vaddr)
-    {
-        auto registrar = static_cast<const MockRegistrarDevice*>(module);
-        return registrar->lock_interface(
-            module, handle, usage, l, t, w, h, vaddr);
-    }
-
-    static int hook_unlock(
-        struct gralloc_module_t const* module, buffer_handle_t handle)
-    {
-        auto registrar = static_cast<const MockRegistrarDevice*>(module);
-        return registrar->unlock_interface(module, handle);
-    }
+    MOCK_METHOD(int, close, (const native_handle_t* h), (override));
 };
 
 struct GrallocRegistrar : public ::testing::Test
 {
     GrallocRegistrar() :
-        mock_module(std::make_shared<testing::NiceMock<MockRegistrarDevice>>()),
+        mock_hybris_gralloc(std::make_shared<testing::NiceMock<mtd::MockHybrisGralloc>>()),
+        mock_native_handle_ops(std::make_shared<testing::NiceMock<MockNativeHandleOps>>()),
         mock_native_buffer(std::make_shared<testing::NiceMock<mtd::MockAndroidNativeBuffer>>())
     {
         stub_package.width = width;
@@ -97,6 +55,20 @@ struct GrallocRegistrar : public ::testing::Test
         stub_package.flags = 0;
         for (auto i = 0; i < stub_package.data_items; i++)
             stub_package.data[i] = (i*3);
+
+        using namespace testing;
+        ON_CALL(*mock_hybris_gralloc, importBuffer(_, _))
+            .WillByDefault(Invoke([](buffer_handle_t handle, buffer_handle_t &out_handle) {
+                // native_handle_clone(), but without dup() call.
+                auto new_handle = native_handle_create(handle->numFds, handle->numInts);
+
+                auto size = sizeof(native_handle_t) +
+                    ((handle->numFds + handle->numInts) * sizeof(int));
+                memcpy(new_handle, handle, size);
+
+                out_handle = new_handle;
+                return 0;
+        }));
     }
 
     uint32_t const width{41};
@@ -107,14 +79,15 @@ struct GrallocRegistrar : public ::testing::Test
     MirPixelFormat const pf{mir_pixel_format_abgr_8888};
     geom::Rectangle const rect{geom::Point{top, left}, geom::Size{width, height}};
 
-    std::shared_ptr<MockRegistrarDevice> const mock_module;
+    std::shared_ptr<mtd::MockHybrisGralloc> const mock_hybris_gralloc;
+    std::shared_ptr<MockNativeHandleOps> const mock_native_handle_ops;
     MirBufferPackage stub_package;
     std::shared_ptr<mtd::MockAndroidNativeBuffer> const mock_native_buffer;
 };
 
 TEST_F(GrallocRegistrar, client_buffer_converts_stub_package)
 {
-    mcla::GrallocRegistrar registrar(mock_module);
+    mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
     auto buffer = registrar.register_buffer(stub_package, pf);
 
     auto handle = buffer->handle();
@@ -129,61 +102,37 @@ TEST_F(GrallocRegistrar, client_buffer_converts_stub_package)
 
 TEST_F(GrallocRegistrar, client_sets_correct_version)
 {
-    mcla::GrallocRegistrar registrar(mock_module);
+    mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
     auto buffer = registrar.register_buffer(stub_package, pf);
     EXPECT_EQ(buffer->handle()->version, static_cast<int>(sizeof(native_handle_t)));
-}
-
-TEST_F(GrallocRegistrar, registrar_registers_using_module)
-{
-    using namespace testing;
-    native_handle_t const* handle1 = nullptr;
-    native_handle_t const* handle2 = nullptr;
-
-    EXPECT_CALL(*mock_module, registerBuffer_interface(mock_module.get(),_))
-        .Times(1)
-        .WillOnce(DoAll(SaveArg<1>(&handle1), Return(0)));
-    EXPECT_CALL(*mock_module, unregisterBuffer_interface(mock_module.get(),_))
-        .Times(1)
-        .WillOnce(DoAll(SaveArg<1>(&handle2), Return(0)));
-
-    mcla::GrallocRegistrar registrar(mock_module);
-    {
-        auto buffer = registrar.register_buffer(stub_package, pf);
-        EXPECT_EQ(handle1, buffer->handle());
-    }
-    EXPECT_EQ(handle1, handle2);
 }
 
 TEST_F(GrallocRegistrar, registrar_frees_fds)
 {
     using namespace testing;
-    MirBufferPackage stub_package;
-    memset(&stub_package, 0, sizeof(MirBufferPackage));
-    stub_package.data_items = 0;
-    stub_package.flags = 0;
-    stub_package.fd_items = 2;
-    EXPECT_EQ(0, pipe(static_cast<int*>(stub_package.fd)));
+
+    EXPECT_CALL(*mock_hybris_gralloc, release(
+            Field(&native_handle_t::numFds, Eq(stub_package.fd_items)),
+            /* was_allocated */ false))
+        .Times(1);
 
     {
-        mcla::GrallocRegistrar registrar(mock_module);
+        mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
         auto buffer = registrar.register_buffer(stub_package, pf);
     }
-    EXPECT_EQ(-1, fcntl(stub_package.fd[0], F_GETFD));
-    EXPECT_EQ(-1, fcntl(stub_package.fd[1], F_GETFD));
 }
 
 
 TEST_F(GrallocRegistrar, register_failure)
 {
     using namespace testing;
-    EXPECT_CALL(*mock_module, registerBuffer_interface(_, _))
+    EXPECT_CALL(*mock_hybris_gralloc, importBuffer(_, _))
         .Times(1)
         .WillOnce(Return(-1));
-    EXPECT_CALL(*mock_module, unregisterBuffer_interface(_,_))
+    EXPECT_CALL(*mock_hybris_gralloc, release(_,_))
         .Times(0);
 
-    mcla::GrallocRegistrar registrar(mock_module);
+    mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
     EXPECT_THROW({
         registrar.register_buffer(stub_package, pf);
     }, std::runtime_error);
@@ -192,37 +141,31 @@ TEST_F(GrallocRegistrar, register_failure)
 TEST_F(GrallocRegistrar, region_is_cleaned_up_correctly)
 {
     using namespace testing;
-    mcla::GrallocRegistrar registrar(mock_module);
+    mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
 
-    gralloc_module_t const* gralloc_dev_alloc{nullptr};
-    gralloc_module_t const* gralloc_dev_freed{nullptr};
     native_handle_t const* handle_alloc{nullptr};
     native_handle_t const* handle_freed{nullptr};
 
     Sequence seq;
-    EXPECT_CALL(*mock_module, lock_interface(_,_,_,_,_,_,_,_))
+    EXPECT_CALL(*mock_hybris_gralloc, lock(_,_,_,_,_,_,_))
         .InSequence(seq)
         .WillOnce(DoAll(
-            SaveArg<0>(&gralloc_dev_alloc),
-            SaveArg<1>(&handle_alloc),
+            SaveArg<0>(&handle_alloc),
             Return(0)));
-    EXPECT_CALL(*mock_module, unlock_interface(_,_))
+    EXPECT_CALL(*mock_hybris_gralloc, unlock(_))
         .InSequence(seq)
         .WillOnce(DoAll(
-            SaveArg<0>(&gralloc_dev_freed),
-            SaveArg<1>(&handle_freed),
+            SaveArg<0>(&handle_freed),
             Return(0)));
 
     registrar.secure_for_cpu(mock_native_buffer, rect);
 
-    EXPECT_EQ(gralloc_dev_freed, gralloc_dev_alloc);
     EXPECT_EQ(handle_alloc, handle_freed);
 }
 
 TEST_F(GrallocRegistrar, maps_buffer_for_cpu_correctly)
 {
-    EXPECT_CALL(*mock_module, lock_interface(
-        mock_module.get(),
+    EXPECT_CALL(*mock_hybris_gralloc, lock(
         mock_native_buffer->handle(),
         GRALLOC_USAGE_SW_READ_OFTEN | GRALLOC_USAGE_SW_WRITE_OFTEN,
         top,
@@ -232,18 +175,18 @@ TEST_F(GrallocRegistrar, maps_buffer_for_cpu_correctly)
         testing::_))
         .Times(1);
 
-    mcla::GrallocRegistrar registrar(mock_module);
+    mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
     registrar.secure_for_cpu(mock_native_buffer, rect);
 }
 
 TEST_F(GrallocRegistrar, lock_failure_throws)
 {
     using namespace testing;
-    EXPECT_CALL(*mock_module, lock_interface(_,_,_,_,_,_,_,_))
+    EXPECT_CALL(*mock_hybris_gralloc, lock(_,_,_,_,_,_,_))
         .Times(1)
         .WillOnce(Return(-1));
 
-    mcla::GrallocRegistrar registrar(mock_module);
+    mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
 
     EXPECT_THROW({
         registrar.secure_for_cpu(mock_native_buffer, rect);
@@ -254,10 +197,10 @@ TEST_F(GrallocRegistrar, lock_failure_throws)
 TEST_F(GrallocRegistrar, unlock_failure_doesnt_throw)
 {
     using namespace testing;
-    EXPECT_CALL(*mock_module, unlock_interface(_,_))
+    EXPECT_CALL(*mock_hybris_gralloc, unlock(_))
         .Times(1)
         .WillOnce(Return(-1));
-    mcla::GrallocRegistrar registrar(mock_module);
+    mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
 
     auto region = registrar.secure_for_cpu(mock_native_buffer, rect);
     EXPECT_NO_THROW({
@@ -268,7 +211,7 @@ TEST_F(GrallocRegistrar, unlock_failure_doesnt_throw)
 TEST_F(GrallocRegistrar, produces_valid_anwb)
 {
     using namespace testing;
-    mcla::GrallocRegistrar registrar(mock_module);
+    mcla::GrallocRegistrar registrar(mock_hybris_gralloc, mock_native_handle_ops);
 
     int correct_usage = GRALLOC_USAGE_HW_TEXTURE | GRALLOC_USAGE_HW_RENDER;
     int32_t const expected_stride_in_pixels = static_cast<int32_t>(stride / MIR_BYTES_PER_PIXEL(pf));

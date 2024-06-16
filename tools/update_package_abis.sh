836e3f2a7d0120b49128d4d771d7149934cf26da
Ratchanan Srirattanamet <ratchanan@ubports.com>
2024-06-17 02:51:20 +0700

    tools/update_package_abis.sh: update for package renames

--- tools/update_package_abis.sh
+++ tools/update_package_abis.sh
@@ -10,8 +10,8 @@ then
 fi
 
 packages="\
-    mir-client-platform-android2-:MIR_CLIENT_PLATFORM_ABI \
-    mir-platform-graphics-android2-:MIR_SERVER_GRAPHICS_PLATFORM_ABI"
+    mir1-client-platform-android2-:MIR_CLIENT_PLATFORM_ABI \
+    mir1-platform-graphics-android2-:MIR_SERVER_GRAPHICS_PLATFORM_ABI"
 
 package_name()
 {
